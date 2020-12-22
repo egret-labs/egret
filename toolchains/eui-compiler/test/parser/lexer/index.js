f5df4c3c42e3b4fa20292c9b3e2d9d7b5f30eefd
zhangjiayi <zhangjiayi@egret.com>
2020-12-22 17:48:20 +0800

    add exml lexer

---
+++ toolchains/eui-compiler/test/parser/lexer/index.js
@@ -0,0 +1,33 @@
+//@ts-check
+const assert = require('assert');
+const fs = require('fs');
+const path = require('path');
+const { describe, it, afterEach } = require('mocha');
+const { Lexer } = require('../../../lib/parser/lexer');
+
+
+
+describe('parser-lexer', () => {
+
+
+    const baselineDir = path.join(__dirname, 'baselines')
+    const dirs = fs.readdirSync(baselineDir)
+    const cwd = process.cwd();
+    afterEach(function () {
+        process.chdir(cwd);
+    });
+    for (const dir of dirs) {
+
+        it(`parser-lexer-${dir}`, () => {
+            process.chdir(path.join(baselineDir, dir));
+            const content = fs.readFileSync('input.exml', 'utf-8');
+            const lexer = new Lexer(content);
+            const result = lexer.analysis();
+            const expect = fs.readFileSync('token.json', 'utf-8');
+            assert.deepEqual(JSON.stringify(result, null, 4), expect);
+
+        })
+
+    }
+
+})
