51850f890a159a74c2b2abf87e1b68729c223c74
WanderWang <wangze@egret.com>
2021-02-02 10:34:27 +0800

    add testcase

--- toolchains/eui-compiler/tests/project/index.js
+++ toolchains/eui-compiler/tests/project/index.js
@@ -22,14 +22,15 @@ const standardTheme = {
         'eui.VSlider': 'resource/eui_skins/VSliderSkin.exml',
         'eui.ItemRenderer': 'resource/eui_skins/ItemRendererSkin.exml'
     },
-    'autoGenerateExmlsList': true,
+    'autoGenerateExmlsList': false,
     'exmls': [
-        'resource/eui_skins/input.exml'
+        'resource/eui_skins/input.exml',
+        'resource/eui_skins/input2.exml'
     ],
     'path': 'resource/default.thm.json'
 };
 
-const inputFile = `
+const inputFile1 = `
 <?xml version='1.0' encoding='utf-8'?>
 <e:Skin class="skins.MyComponent1" width="400"
 	xmlns:e="http://ns.egret.com/eui">
@@ -40,9 +41,17 @@ const inputFile = `
 </e:Skin>
 `;
 
+const inputFile2 = `
+<?xml version='1.0' encoding='utf-8'?>
+<e:Skin class="skins.MyComponent2" width="400"
+	xmlns:e="http://ns.egret.com/eui">
+	<e:Image id="image" width="100" source="a_png" includeInLayout="true"  scale9Grid="1,1,1,1"></e:Image>
+</e:Skin>
+`
+
 describe('完整工程', () => {
 
-    it('最简单的Demo', () => {
+    it('多次输出的结果一致', () => {
 
         const property = path.join(process.cwd(), 'property.json');
         const propertyContent = fs.readFileSync(property);
@@ -53,7 +62,8 @@ describe('完整工程', () => {
                 'resource': {
                     'default.thm.json': JSON.stringify(standardTheme),
                     'eui_skins': {
-                        'input.exml': inputFile
+                        'input.exml': inputFile1,
+                        'input2.exml': inputFile2
                     }
                 }
             },
@@ -63,6 +73,40 @@ describe('完整工程', () => {
         const firstEmitResult = compiler.emit();
         const secondEmitResult = compiler.emit();
         assert.strictEqual(firstEmitResult[0].content, secondEmitResult[0].content);
+        mock.restore();
+    });
+
+    it('ddd', () => {
+
+        const property = path.join(process.cwd(), 'property.json');
+        const propertyContent = fs.readFileSync(property);
+        mock({
+
+            './a/': {
+                'egretProperties.json': JSON.stringify(standardEgretProperties),
+                'resource': {
+                    'default.thm.json': JSON.stringify(standardTheme),
+                    'eui_skins': {
+                        'input.exml': inputFile1,
+                        'input2.exml': inputFile2
+                    }
+                }
+            },
+            [property]: propertyContent
+        });
+        const compiler = new lib.EuiCompiler('./a', 'debug');
+        compiler.emit();
+        const firstEmitResult = compiler.emit();
+        const expectResult = `declare module skins {
+    class MyComponent1 extends eui.Skin {
+    }
+}
+declare module skins {
+    class MyComponent2 extends eui.Skin {
+    }
+}
+`
+        assert.strictEqual(firstEmitResult[0].content, expectResult)
         mock.restore();
     });
 });
