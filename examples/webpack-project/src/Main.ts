917216658d9ef0974156457ff33a1c2c5f7d0e84
WanderWang <wangze@egret.com>
2021-02-05 20:40:13 +0800

    add audio example

--- examples/webpack-project/src/Main.ts
+++ examples/webpack-project/src/Main.ts
@@ -1,8 +1,20 @@
-class Main {
+import { RES } from '@egret/assetsmanager/dist/legacy'
+import { AudioFactory, WebAudioInstance } from '@egret/audio';
+
+class Main extends egret.DisplayObject {
 
     constructor() {
+        super();
         console.log(i18n.name);
+        this.run();
 
 
+    }
+
+    async run() {
+        await RES.loadConfig('default.res.json', 'resource');
+        const audioFactory = await RES.getResAsync("bg_mp3") as AudioFactory<WebAudioInstance>;
+        const audio = audioFactory.create();
+        audio.play();
     }
 }
