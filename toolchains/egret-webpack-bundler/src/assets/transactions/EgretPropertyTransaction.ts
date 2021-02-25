51431111e04def2a12a3ce41bad29c7acafc96be
WanderWang <wangze@egret.com>
2021-02-25 17:21:30 +0800

    fix subpackage issue

--- toolchains/egret-webpack-bundler/src/assets/transactions/EgretPropertyTransaction.ts
+++ toolchains/egret-webpack-bundler/src/assets/transactions/EgretPropertyTransaction.ts
@@ -28,7 +28,6 @@ export class EgretPropertyTransaction extends Transaction {
         const project = new EgretProjectData();
         const content = await manager.inputFileSystem.readFileAsync('egretProperties.json');
         project.initialize(manager.projectRoot, content);
-        // const project = createProject(manager.projectRoot);
         const egretModules = project.getModulesConfig('web');
         const initial: string[] = [];
         for (const m of egretModules) {
@@ -38,8 +37,9 @@ export class EgretPropertyTransaction extends Transaction {
             }
         }
 
-
-        const manifest = { initial, game: ['main.js'] };
+        const subpackages = this.options.subpackages?.map(item => item.name + '.js');
+        const game = subpackages ? ['main.js'].concat(subpackages) : ['main.js'];
+        const manifest = { initial, game };
         const manifestContent = JSON.stringify(manifest, null, '\t');
         manager.outputFileSystem.emitAsset('manifest.json', manifestContent);
     }
